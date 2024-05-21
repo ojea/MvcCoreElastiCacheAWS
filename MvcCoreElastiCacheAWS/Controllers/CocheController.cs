9c471902d4347b0e633ed377df729ceb5955561c
ojea <ojeaxe@gmail.com>
2024-05-21 13:25:44 +0200

    Add project files.

---
+++ MvcCoreElastiCacheAWS/Controllers/CocheController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcCoreElastiCacheAWS.Models;
+using MvcCoreElastiCacheAWS.Repositories;
+
+namespace MvcCoreElastiCacheAWS.Controllers
+{
+    public class CocheController: Controller
+    {
+        private RepositoryCoches repo;
+
+        public CocheController(RepositoryCoches repo)
+        {
+            this.repo = repo;
+        }
+
+        public IActionResult Index()
+
+        {
+            List<Coche> coches = this.repo.GetCoches();
+            return View(coches);
+        }
+
+        public IActionResult Details(int id)
+        {
+            Coche coche = this.repo.FindCoche(id);
+            return View(coche);
+        }
+    }
+}
---
+++ MvcCoreElastiCacheAWS/Helpers/HelperCacheRedis.cs
@@ -0,0 +1,20 @@
+﻿using StackExchange.Redis;
+
+namespace MvcCoreElastiCacheAWS.Helpers
+{
+    public class HelperCacheRedis
+    {
+        private static Lazy<ConnectionMultiplexer> CreateConnection = new Lazy<ConnectionMultiplexer>(() =>
+        {
+            return ConnectionMultiplexer.Connect("");
+        });
+
+        public ConnectionMultiplexer Connection
+        {
+            get
+            {
+                return CreateConnection.Value;
+            }
+        }
+    }
+}
